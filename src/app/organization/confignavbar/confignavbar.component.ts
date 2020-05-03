d52e9e76225add822880260642c0d985eae59e97
jagadeeswara.urlana <jagadeeswara.urlana@nextsphere.com>
2020-05-03 13:52:26 +0530

    Organization related screens added

---
+++ src/app/organization/confignavbar/confignavbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confignavbar',
+  templateUrl: './confignavbar.component.html',
+  styleUrls: ['./confignavbar.component.scss']
+})
+export class ConfignavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
