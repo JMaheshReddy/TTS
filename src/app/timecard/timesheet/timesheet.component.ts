b4dfca2a6358573ded5152aa6a4cdea67f677454
jagadeeswara.urlana <jagadeeswara.urlana@nextsphere.com>
2020-05-05 15:24:11 +0530

    Time card changes done

--- src/app/timecard/timesheet/timesheet.component.ts
+++ src/app/timecard/timesheet/timesheet.component.ts
@@ -7,9 +7,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimesheetComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log(this.getWeekDays());
+  }
+  weekDays = [];
+  currentDate = new Date();
+  selectedDate = this.currentDate;
 
   ngOnInit() {
   }
 
-}
+  getWeekDays() {
+    var oneDay = 24 * 60 * 60 * 1000;
+    var inputDate = new Date();
+    inputDate = new Date(inputDate.toDateString());
+    var minusDate = inputDate.getDay() === 0 ? 6 : (inputDate.getDay() - 1);
+    var startDayOfWeek = new Date(new Date(inputDate).getTime() - minusDate * oneDay);
+    this.generateWeekDays(startDayOfWeek);
+  }
+
+  generateWeekDays(startDate) {
+    var oneDay = 24 * 60 * 60 * 1000;
+    var dupDay = new Date();
+    var i = 1;
+    this.weekDays.push(startDate);
+    while (i <= 6) {
+      dupDay = new Date(new Date(startDate).getTime() + oneDay);
+      this.weekDays.push(dupDay);
+      startDate = dupDay;
+      i++;
+    };
+  };
+
+  changeWeek(isNextWeek) {
+    debugger
+    var oneDay = 24 * 60 * 60 * 1000;
+    var inputDate;
+    var currentWeekDate;
+    if (this.weekDays != undefined && this.weekDays.length > 0) {
+      currentWeekDate = this.weekDays[0];
+    }
+    else {
+      currentWeekDate = new Date();
+    }
+    if (isNextWeek) {
+      inputDate = new Date(currentWeekDate.getTime() + 7 * 24 * 60 * 60 * 1000);
+    }
+    else {
+      inputDate = new Date(currentWeekDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+    }
+    this.weekDays = [];
+
+    inputDate = new Date(inputDate.toDateString());
+    var minusDate = inputDate.getDay() === 0 ? 6 : (inputDate.getDay() - 1);
+    var startDayOfWeek = new Date(new Date(inputDate).getTime() - minusDate * oneDay);
+    this.generateWeekDays(startDayOfWeek);
+  };
+
+  getSelectedDateRecords = function(selectedDate) {
+    this.selectedDate = selectedDate;
+  }
+};
